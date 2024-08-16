5f3cb39ea1f6697e7103b30ea9a0bb5e7d80eaee
Brandon Gonzalez <brajgon@gmail.com>
2024-08-15 23:30:39 -0600

    added event listener, click

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,6 +13,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  // the below -- now all properties this is available in the user component
   selectedUser = DUMMY_USERS[randomIndex];
-  // the above -- now all properties this is available in the user component
+
+  // outputting dynamically the photos to the user.component.html file
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log('Clicked')
+  }
 }
