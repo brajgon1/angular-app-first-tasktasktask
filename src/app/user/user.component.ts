e61adff10d4bea9a51d57236fc95eafdfa7578db
Brandon Gonzalez <brajgon@gmail.com>
2024-08-15 23:11:14 -0600

    added user dynamically

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+// Below - how to pull a random user from the dummy users list
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +13,6 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex];
+  // the above -- now all properties this is available in the user component
 }
