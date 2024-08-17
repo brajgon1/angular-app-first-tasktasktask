eacdf9033f50eca6863882788fa618fdd1752df4
Brandon Gonzalez <brajgon@gmail.com>
2024-08-17 15:07:03 -0600

    using signal inputs 32

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,6 +10,14 @@ import { Component, Input } from '@angular/core';
 export class UserComponent {
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+
+  // COMMENTED CODE --- this is how you would use signal inputs - and then in the user,component.html you would invoke
+  // avatar = input.required<string>()
+  // name = input.required<string>()
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // })
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
