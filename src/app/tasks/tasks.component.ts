4eb03f381448ac82835e1c1b10588348ae39bf6f
Brandon Gonzalez <brajgon@gmail.com>
2024-08-17 23:07:14 -0600

    working on outputing names when clicked on dynamically

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
   selector: 'app-tasks',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
 
+  users = DUMMY_USERS.find;
+
+  onSelectUser() {
+    this.select.emit(this.name);
+  }
 }
