a5717939adce668710cb12ea003e9aefa93a213d
Brandon Gonzalez <brajgon@gmail.com>
2024-08-26 19:49:21 -0600

    declaring & using components 68

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core"; 
+import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
 @NgModule({
     declarations: [AppComponent],
     bootstrap: [AppComponent],
+    imports: [BrowserModule, HeaderComponent, TasksComponent, UserComponent]
 })
 export class AppModule {}
